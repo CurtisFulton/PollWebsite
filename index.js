bb8626048f5fb5f6dec167e4afa3ad12fd392d53
Curtis Fulton <Curtis.R.Fulton@Gmail.com>
2017-08-08 14:44:36 +1000

    Fix errors on submitting poll
    There were some errors with scripts not properly running.
    
    But most importantly express-validator was no longer on the server so I
    added it again which fixed most problems.
    
    It seems that duplication checking still does not work.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 const app = express();
 
@@ -10,11 +11,62 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+app.use(function(req, res, next) {
+	res.locals.errors = null;
+	next();
+});
+
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressValidator());
 
 const apiRouter = require('./routes/poll-api.js');
 const pollRouter = require('./routes/poll.js');
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use(expressValidator({
+	customValidators: {
+		isArray: function(value) {
+			return Array.isArray(value)
+		},
+		validStrings: function(array, value) {
+			var valid = 0;
+			
+			array.forEach(function(val){
+				if (val != null && val.length > 0)
+					valid++;
+			});
+
+			return valid >= value;
+		},
+		eachNotEmpty: function(values) {
+			return values.every(function(val){
+				return val != null && val.length > 0;
+			});
+		}
+	}
+}));
 
 // Default portfolio page
 app.get('/', function(req, res, next) {
@@ -25,5 +77,11 @@ app.get('/', function(req, res, next) {
 
 app.use('/api', apiRouter);
 app.use('/polls', pollRouter);
+
+
+app.listen(8080, function() {
+	console.log('Server Online. Port: ' + 8080);
+});
+
 
 exports.app = app;
